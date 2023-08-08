f2b484f379510199b8477e206ebd2f877566238c
BohdanPlastun <bohdan.plastun@lanars.com>
2023-08-08 16:22:49 +0300

    Remove phone form field widget separation

--- lib/src/widgets/phone_form_field.dart
+++ lib/src/widgets/phone_form_field.dart
@@ -3,17 +3,13 @@ import 'dart:ui' as ui show BoxHeightStyle, BoxWidthStyle;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:phone_form_field/phone_form_field.dart';
+import 'package:phone_form_field/src/constants/patterns.dart';
+import 'package:phone_form_field/src/controllers/phone_field_controller.dart';
+import 'package:phone_form_field/src/validation/validator_translator.dart';
 import 'package:phone_numbers_parser/phone_numbers_parser.dart';
 
-import '../constants/patterns.dart';
-import '../controllers/phone_controller.dart';
-import '../controllers/phone_field_controller.dart';
-import '../validation/phone_validator.dart';
-import '../validation/validator_translator.dart';
-import 'country_selector/country_selector_navigator.dart';
 import 'phone_field.dart';
-
-part 'phone_form_field_state.dart';
 
 /// Phone input extending form field.
 ///
@@ -50,15 +46,6 @@ part 'phone_form_field_state.dart';
 /// with nsn of 677784455.
 /// {@endtemplate}
 ///
-/// ### phoneNumberType:
-/// {@template phoneNumberType}
-/// specify the type of phone number with [phoneNumberType].
-///
-/// accepted values are:
-///   - null (can be mobile or fixedLine)
-///   - mobile
-///   - fixedLine
-/// {@endtemplate}
 ///
 ///
 /// ### Country picker:
@@ -72,13 +59,6 @@ part 'phone_form_field_state.dart';
 ///  - const ModalBottomSheetNavigator()
 ///  - const DialogNavigator()
 /// {@endtemplate}
-///
-/// ### Country Code visibility:
-///
-/// The country dial code will be visible when:
-/// - the field is focussed.
-/// - the field has a value for national number.
-/// - the field has no label obstructing the view.
 class PhoneFormField extends FormField<PhoneNumber> {
   /// {@macro controller}
   final PhoneController? controller;
@@ -95,9 +75,6 @@ class PhoneFormField extends FormField<PhoneNumber> {
   /// the focusNode of the national number
   final FocusNode? focusNode;
 
-  /// show Dial Code or not
-  final bool showDialCode;
-
   PhoneFormField({
     Key? key,
     this.controller,
@@ -109,8 +86,7 @@ class PhoneFormField extends FormField<PhoneNumber> {
         const CountrySelectorNavigator.searchDelegate(),
     Function(PhoneNumber?)? onSaved,
     this.defaultCountry = IsoCode.US,
-    InputDecoration decoration =
-        const InputDecoration(border: UnderlineInputBorder()),
+    InputDecoration decoration = const InputDecoration(border: UnderlineInputBorder()),
     AutovalidateMode autovalidateMode = AutovalidateMode.onUserInteraction,
     PhoneNumber? initialValue,
     double flagSize = 20,
@@ -156,7 +132,6 @@ class PhoneFormField extends FormField<PhoneNumber> {
     Iterable<String>? autofillHints,
     String? restorationId,
     bool enableIMEPersonalizedLearning = true,
-    this.showDialCode = true,
   })  : assert(
           initialValue == null || controller == null,
           'One of initialValue or controller can be specified at a time',
@@ -176,7 +151,6 @@ class PhoneFormField extends FormField<PhoneNumber> {
               showFlagInInput: showFlagInInput,
               selectorNavigator: countrySelectorNavigator,
               errorText: field.getErrorText(),
-              showDialCode: showDialCode,
               isFlagCircle: isFlagCircle,
               flagSize: flagSize,
               decoration: decoration,
@@ -226,4 +200,117 @@ class PhoneFormField extends FormField<PhoneNumber> {
 
   @override
   PhoneFormFieldState createState() => PhoneFormFieldState();
+}
+
+class PhoneFormFieldState extends FormFieldState<PhoneNumber> {
+  late final PhoneController _controller;
+  late final PhoneFieldController _childController;
+  late final StreamSubscription<void> _selectionSubscription;
+
+  @override
+  PhoneFormField get widget => super.widget as PhoneFormField;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? PhoneController(value);
+    _childController = PhoneFieldController(
+      isoCode: _controller.value?.isoCode ?? widget.defaultCountry,
+      national: _getFormattedNsn(),
+      focusNode: widget.focusNode ?? FocusNode(),
+    );
+    _controller.addListener(_onControllerChange);
+    _childController.addListener(() => _onChildControllerChange());
+    // to expose text selection of national number
+    _selectionSubscription = _controller.selectionRequestStream
+        .listen((event) => _childController.selectNationalNumber());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _childController.dispose();
+    _selectionSubscription.cancel();
+    _controller.removeListener(_onControllerChange);
+    // dispose the controller only when it's initialised in this instance
+    // otherwise this should be done where instance is created
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+  }
+
+  @override
+  void reset() {
+    _controller.value = widget.initialValue;
+    super.reset();
+  }
+
+  /// when the controller changes this function will
+  /// update the childController so the [PhoneField] which
+  /// deals with the UI can display the correct value.
+  void _onControllerChange() {
+    final phone = _controller.value;
+
+    widget.onChanged?.call(phone);
+    didChange(phone);
+    final formatted = _getFormattedNsn();
+    if (_childController.national != formatted) {
+      _childController.national = formatted;
+    }
+    if (_childController.isoCode != phone?.isoCode) {
+      _childController.isoCode = phone?.isoCode ?? widget.defaultCountry;
+    }
+  }
+
+  /// when the base controller changes (when the user manually input something)
+  /// then we need to update the local controller's value.
+  void _onChildControllerChange() {
+    if (_childController.national == _controller.value?.nsn &&
+        _childController.isoCode == _controller.value?.isoCode) {
+      return;
+    }
+
+    if (_childController.national == null) {
+      return _controller.value = null;
+    }
+    // we convert the multiple controllers from the child controller
+    // to a full blown PhoneNumber to access validation, formatting etc.
+    PhoneNumber phoneNumber;
+    // when the nsn input change we check if its not a whole number
+    // to allow for copy pasting and auto fill. If it is one then
+    // we parse it accordingly.
+    // we assume it's a whole phone number if it starts with +
+    final childNsn = _childController.national;
+    if (childNsn != null && childNsn.startsWith(RegExp('[${Patterns.plus}]'))) {
+      // if starts with + then we parse the whole number
+      // to figure out the country code
+      final international = childNsn;
+      try {
+        phoneNumber = PhoneNumber.parse(international);
+      } on PhoneNumberException {
+        return;
+      }
+    } else {
+      phoneNumber = PhoneNumber.parse(
+        childNsn ?? '',
+        destinationCountry: _childController.isoCode,
+      );
+    }
+    _controller.value = phoneNumber;
+  }
+
+  String? _getFormattedNsn() {
+    if (widget.shouldFormat) {
+      return _controller.value?.getFormattedNsn();
+    }
+    return _controller.value?.nsn;
+  }
+
+  /// gets the localized error text if any
+  String? getErrorText() {
+    if (errorText != null) {
+      return ValidatorTranslator.message(context, errorText!);
+    }
+    return null;
+  }
 }
